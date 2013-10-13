a2a407f133157851437cfd997964242b241a054e
PratapVhatkar <pratapvhatkar1989@gmail.com>
2013-10-13 08:59:01 +0530

    first commit

---
+++ CoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  CoreViewController.h
+//  Core_Data
+//
+//  Created by Akshay Vhatkar on 14/09/13.
+//  Copyright (c) 2013 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoreViewController : UIViewController
+
+@end
